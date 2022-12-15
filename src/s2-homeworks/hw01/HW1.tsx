8eb2c814dbe83cfc356230d8bc94e8c6507bfcd7
Alexandr <paparazzqq@gmail.com>
2022-12-15 15:39:20 +0300

    hr styling

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -58,6 +58,7 @@ const HW1 = () => {
     return (
         <div id={'hw1'}>
             <div className={s2.hwTitle}>Homework #1</div>
+            <hr/>
             <div className={s2.hw}>
                 {/*проверка отображения (не менять)*/}
                 <div>
@@ -68,6 +69,7 @@ const HW1 = () => {
                 {/*для автоматической проверки дз (не менять)*/}
                 <MessageSender M={Message} />
             </div>
+            <hr/>
         </div>
     )
 }
